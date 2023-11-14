2d684a78482b466747421e3bbe442c292f23ece0
Jhonson-96 <yapwilung96@gmail.com>
2023-11-14 23:22:23 +0700

    commit ke 4

--- src/script.js
+++ src/script.js
@@ -1,29 +1,52 @@
-// jalan ketika load
-document.addEventListener("DOMContentLoaded", function () {
-    revealHidden(); // Panggil fungsi ini saat DOMContentLoaded
-});
+const btnNav = document.getElementById('btn-nav');
+const headerNav = document.querySelector('header');
 
-// jalan ketika scroll
-document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("scroll", revealHidden);
-});
-
-function revealHidden() {
-    var elements = document.querySelectorAll(".hidden");
-    for (var i = 0; i < elements.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = elements[i].getBoundingClientRect().top;
-        var distanceFromTop = elementTop - windowHeight + 100;
-
-        if (distanceFromTop < 0) {
-            elements[i].classList.add("visible");
-        }
+btnNav.addEventListener('click', () => {
+    if (headerNav.style.left === '0px') {
+        closeNavBar();
+    } else {
+        openNavBar();
     }
+});
+
+function openNavBar() {
+    headerNav.style.left = '0';
 }
 
-// <div class="container">
-// <div class="content">
-//     <h1 class="hidden">Judul Konten</h1>
-//     <p class="hidden">Isi konten Anda di sini.</p>
-// </div>
-// </div>
+function closeNavBar() {
+    headerNav.style.left = '-20rem';
+}
+
+const menuItems = document.querySelectorAll('nav a');
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        closeNavBar();
+    });
+});
+
+
+
+
+// // jalan ketika load
+// document.addEventListener("DOMContentLoaded", function () {
+//     revealHidden(); // Panggil fungsi ini saat DOMContentLoaded
+// });
+
+// // jalan ketika scroll
+// document.addEventListener("DOMContentLoaded", function () {
+//     window.addEventListener("scroll", revealHidden);
+// });
+
+// function revealHidden() {
+//     var elements = document.querySelectorAll(".hidden");
+//     for (var i = 0; i < elements.length; i++) {
+//         var windowHeight = window.innerHeight;
+//         var elementTop = elements[i].getBoundingClientRect().top;
+//         var distanceFromTop = elementTop - windowHeight + 100;
+
+//         if (distanceFromTop < 0) {
+//             elements[i].classList.add("visible");
+//         }
+//     }
+// }
+
